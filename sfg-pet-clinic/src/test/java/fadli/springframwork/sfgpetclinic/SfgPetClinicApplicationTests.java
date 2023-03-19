e4a3106d95c27695286ae75d2af716135acb71c0
fadliyandra <fadliyandra27.fy@gmail.com>
2023-03-19 19:30:52 +0700

    Initial Project Add

---
+++ sfg-pet-clinic/src/test/java/fadli/springframwork/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package fadli.springframwork.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
